7e50cb3a140530f66130a13eba2eab15e76c1c6a
M-sahand-dev <leymehdi5@gmail.com>
2025-08-20 19:50:15 +0330

    add font style Pelak and icon logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello Vite + React!</h1>
-    </>
+    <div className=" bg-white py-10 px-12">
+      <h1 className="text-3xl font-bold font-Pelak">سلام</h1>
+      <h1 className="text-3xl font-medium font-Pelak">سلام</h1>
+      <h1 className="text-3xl font-Pelak">سلام</h1>
+    </div>
   );
 }
 
